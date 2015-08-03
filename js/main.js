5886f7de5541c32870bae67dde327e2837c5e4a9
amberwoodrow <amberwoodrow@gmail.com>
2015-08-02 19:03:07 -0600

    add comments

--- js/main.js
+++ js/main.js
@@ -1,53 +1,53 @@
-var toBeCalculated = [];
-var justCalculated = false;
+var toBeCalculated = []; // array of buttons pressed
+var justCalculated = false; // checks to see if array has just been calculated
 
+// deals with eval of string
 function equals () {
-  //console.log(toBeCalculated);
-  var calcStr = toBeCalculated.join(" ");
-  if (calcStr.match(/[0-9 +-\/*]/)) {
-    justCalculated = true;
+  var calcStr = toBeCalculated.join(" "); // turns toBeCalculated to string to be evaled by .match and eval
+  if (calcStr.match(/[0-9 +-\/*]/)) { // makes eval safe by making sure the string is only ints, operators and spaces.
+    justCalculated = true; 
     return eval(calcStr);
   }
-  return [""];
+  return [""]; // empty string if string entered for eval does not match regex above
 }
 
-
-$('#zero').click(function() {
-  toBeCalculated.push(0);
+// I used ids to grab each number, or operator clicked and put it on the screen
+$('#zero').click(function() { // id of 'zero' shows up on screen and array as 'zero', so here I caught it before 
+  toBeCalculated.push(0);     // it is pushed to the array and push 0 instead
 });
 
-$('#cancel').click(function() {
+$('#cancel').click(function() { // empties array toBeCalculated on click of cancel id
   toBeCalculated = "";
 });
 
-$('#calc').click(function() {
-  toBeCalculated = [equals().toString()];
-  $('#screen').html(toBeCalculated);
+$('#calc').click(function() { // calls the equals function to calc the array
+  toBeCalculated = [equals().toString()]; // creates array with one element inside
+  $('#screen').html(toBeCalculated); // sticks the calculated array on the screen
 });
 
-$('span:not([id=calc], [id=cancel], [id=zero])').click(function() {
-  toBeCalculated.push(event.target.id);
+$('span:not([id=calc], [id=cancel], [id=zero])').click(function() { // displays all ids on screen except ones
+  toBeCalculated.push(event.target.id);                             // mentioned
 });
 
-$('span:not([id=calc]').click(function() {
-
-    if (justCalculated === true) {
-      var lastStr = toBeCalculated.slice(-1).toString();
-      if (lastStr.match(/[0-9]/)) {
-        var last = toBeCalculated.slice(-1);
-        toBeCalculated = "";
-        toBeCalculated = last;
-        justCalculated = false;
-      }
-      else {
-        justCalculated = false;
-      }
+$('span:not([id=calc]').click(function() { // puts span clicked on screen
+  // checks if array was just evaled. If I didn't check the result of the last evaluated array would stay on the
+  // screen and a new number would be added and it would not know how to eval it. ex:
+  //  6 being from last eval result: 6 5 + 3 can't eval 5 space 6
+  if (justCalculated === true) { 
+    var lastStr = toBeCalculated.slice(-1).toString(); // captures last span clicked and turns it to a string to be matched.
+    if (lastStr.match(/[0-9]/)) { // checks to see if the span just clicked is a number
+      var last = toBeCalculated.slice(-1); // stores last #
+      toBeCalculated = ""; // empties array
+      toBeCalculated = last; // reassigns last # to the array
+      justCalculated = false; // makes false for next eval to function properly
     }
+    else { // allows you to operate on the last evaled result
+      justCalculated = false; 
+    }
+  }
 
-  $('#screen').html(toBeCalculated);
+  $('#screen').html(toBeCalculated); // puts span clicked on screen
 });
 
-$("#screen").html("alert('lol')");
-$("#calc").click();
-
-//if next sting after eval has id of 0-9 clear toBeCalculated
+$("#screen").html("alert('lol')"); // tests eval safty regex
+$("#calc").click(); // tests eval safty regex

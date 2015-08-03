3e3bbab4dc0c9d3e6f0089cf482566079df3f273
amberwoodrow <amberwoodrow@gmail.com>
2015-08-02 18:30:56 -0600

    fix bug: after eval if a number was pressed it had no way to concat string, made eval function safe with regex

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 var toBeCalculated = [];
+var justCalculated = false;
 
 function equals () {
-  console.log(toBeCalculated);
+  //console.log(toBeCalculated);
   var calcStr = toBeCalculated.join(" ");
-  console.log((calcStr));
-  console.log(eval(calcStr));
-  return eval(calcStr);
+  if (calcStr.match(/[0-9 +-\/*]/)) {
+    justCalculated = true;
+    return eval(calcStr);
+  }
+  return [""];
 }
+
 
 $('#zero').click(function() {
   toBeCalculated.push(0);
@@ -25,6 +29,25 @@ $('span:not([id=calc], [id=cancel], [id=zero])').click(function() {
   toBeCalculated.push(event.target.id);
 });
 
-$('span, span:not([id=calc]').click(function() {
+$('span:not([id=calc]').click(function() {
+
+    if (justCalculated === true) {
+      var lastStr = toBeCalculated.slice(-1).toString();
+      if (lastStr.match(/[0-9]/)) {
+        var last = toBeCalculated.slice(-1);
+        toBeCalculated = "";
+        toBeCalculated = last;
+        justCalculated = false;
+      }
+      else {
+        justCalculated = false;
+      }
+    }
+
   $('#screen').html(toBeCalculated);
 });
+
+$("#screen").html("alert('lol')");
+$("#calc").click();
+
+//if next sting after eval has id of 0-9 clear toBeCalculated
